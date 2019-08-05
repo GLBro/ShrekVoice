93119bae6d835d212cb8c94a2524fb713d0b3f7d
GLBro <glebon2nrt@nsix.org.uk>
2019-08-05 16:12:20 +0100

    more words

--- shrek.py
+++ shrek.py
@@ -4,6 +4,8 @@ while True:
     answer = input('What would you like to ask: ')
     if "song" in answer:
         respond = "We will play a song"
+    elif "swamp" in answer:
+        respond = "Get out of my swamp!"
     else:
         respond = random.choice(response)
     print(respond)
