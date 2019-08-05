c8a879b59e87e7f3dcd56bbfdc36984d45e170eb
GLBro <glebon2nrt@nsix.org.uk>
2019-08-05 15:48:18 +0100

    Basic chatbot

---
+++ shrek.py
@@ -0,0 +1,6 @@
+import random
+while True:
+    answer = input('What would you like to ask: ')
+    response = 'GET OUT OF MY SWAMP', 'NO', 'Ogres are like onions'
+    respond = random.choice(response)
+    print(respond)
