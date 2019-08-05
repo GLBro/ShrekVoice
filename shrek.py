86e9840649283acb02eebf72f25d14dd40bd45fc
GLBro <glebon2nrt@nsix.org.uk>
2019-08-05 16:07:54 +0100

    Specific words

--- shrek.py
+++ shrek.py
@@ -2,5 +2,8 @@ import random
 response = open("soundclips/shrekquotes", "r").readlines()
 while True:
     answer = input('What would you like to ask: ')
-    respond = random.choice(response)
+    if "song" in answer:
+        respond = "We will play a song"
+    else:
+        respond = random.choice(response)
     print(respond)
