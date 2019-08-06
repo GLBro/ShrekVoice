5844f93315eba62ef63fa33454c90ea6825586e3
GLBro <glebon2nrt@nsix.org.uk>
2019-08-06 09:14:07 +0100

    extra

--- shrek.py
+++ shrek.py
@@ -18,6 +18,8 @@ while True:
         respond = "its on my to do list"
     elif "princess" in answer or 'fiona' in answer:
         respond = "the princess will be up in the stairs in the highest room in the tallest tower"
+    elif "ogre" in answer:
+        respond = "ogres are like onions. onions have layers. ogres have layers"
     else:
         respond = random.choice(response)
     
