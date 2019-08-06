9aa03fa9ff1951ee576dde4855197efefbcb70db
Rupert156 <themakers156@gmail.com>
2019-08-06 15:21:51 +0100

    improved layout ready to collab with speech recogniton

--- shrekmenu.py
+++ shrekmenu.py
@@ -1,20 +1,38 @@
 from tkinter import *
+#from PIL import Image, ImageTk
 
 root = Tk()
 root.title("Shrek chat box")
-root.geometry('300x300')
-inputText= Entry().grid(row=8)
-outputText= Entry().grid(row=10)
+root.resizable(False, False)
+shrekvoice =Entry()
+shrekvoice.grid()
 
-
-
-photo = PhotoImage(file="shrek is peng.png")
-label = Label(root, image=photo)
-label.grid(row=10)
+#func for text input
 def buttonFunction():
     print("Shrek")
 
-b = Button(root, text="enter!", command=buttonFunction)
+#bg image
+shrekImage = PhotoImage(file="shrek is peng.png" )
+shreklabel = Label(root, image=shrekImage)
+shreklabel.grid(row=0)
+
+#speech bubble
+speechImage = PhotoImage(file="speechBubble.png" )
+speechlabel = Label(root, image=speechImage)
+speechlabel.place(x=0, y=0)
+
+#response
+respond = "shrek"
+output = Label(text=respond) #prints value of 'respond' variable to the label!
+output.place(x=80, y= 70)
+
+#mic button
+micButton=Button(root)
+micImage=PhotoImage(file="mic.png")
+micButton.config(image=micImage,width="100",height="100", bd=0,command=buttonFunction)
+micButton.grid(row=13)
+
+b = Button(root, text="enter!", command=buttonFunction) #text input button 
 b.grid(row=1)
 
 root.mainloop()
