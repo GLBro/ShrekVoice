5192c2f5f4ce63695da81c457178cb3f1223647c
GLBro <glebon2nrt@nsix.org.uk>
2019-08-06 14:59:05 +0100

    working speech recog

--- shrek.py
+++ shrek.py
@@ -1,38 +1,48 @@
 import random
 import speech_recognition as sr
+import time
 sr.__version__
 r = sr.Recognizer()
 mic = sr.Microphone(device_index=0)
-[
-    'Microphone (Realtek Audio)'
-]
+
 
 response = open("soundclips/shrekquotes", "r").readlines()
-while True:
-    with mic as source:
-        r.adjust_for_ambient_noise(source)
+
+
+with mic as source:
+    r.dynamic_energy_threshold = False
+    print('>>> ambient noise')
+    r.adjust_for_ambient_noise(source)
+    while True:
+        print('>>> listening')
         audio = r.listen(source)
-        print(r.recognize_google(audio))
-        answer = r.recognize_google(audio)  #input('What would you like to ask: ')
-    if "song" in answer:
-        respond = "We will play a song"
-    elif "swamp" in answer:
-        rad = "Get out of my swamp!", "that would be my home"
-        respond = random.choice(rad)
-    elif "book" in answer:
-        respond = "i read a book once"
-    elif "annoying" in answer:
-        rad = "could you be quiet just FOR 5 MINUTES!", "two things ok?Shut..up"
-        respond = random.choice(rad)
-    elif "story" in answer: 
-        respond= "Once upon a time, there was a lovely princess. But she had an enchantment upon her of a fearful sort which could only be broken by love's first kiss. She was locked away in a castle guarded by a terrible fire-breathing dragon. Many brave knights had attempted to free her from this dreadful prison, but none prevailed. She waited in the dragon's keep, in the highest room of the tallest tower, for her true love, and true love's first kiss."
-    elif "remind" in answer:
-        respond = "its on my to do list"
-    elif "princess" in answer or 'fiona' in answer:
-        respond = "the princess will be up in the stairs in the highest room in the tallest tower"
-    elif "ogre" in answer:
-        respond = "ogres are like onions. onions have layers. ogres have layers"
-    else:
-        respond = random.choice(response)
-    
-    print(respond)
+        try:
+            print('>>> sending to google')
+            answer = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print('>>> unknown')
+            continue
+        print(answer)  #input('What would you like to ask: ')
+            #answer = recognize_speech_from_mic(r, mic)
+        if "song" in answer:
+            respond = "We will play a song"
+        elif "swamp" in answer:
+            rad = "Get out of my swamp!", "that would be my home"
+            respond = random.choice(rad)
+        elif "book" in answer:
+            respond = "i read a book once"
+        elif "annoying" in answer:
+            rad = "could you be quiet just FOR 5 MINUTES!", "two things ok?Shut..up"
+            respond = random.choice(rad)
+        elif "story" in answer: 
+            respond= "Once upon a time, there was a lovely princess. But she had an enchantment upon her of a fearful sort which could only be broken by love's first kiss. She was locked away in a castle guarded by a terrible fire-breathing dragon. Many brave knights had attempted to free her from this dreadful prison, but none prevailed. She waited in the dragon's keep, in the highest room of the tallest tower, for her true love, and true love's first kiss."
+        elif "remind" in answer:
+            respond = "its on my to do list"
+        elif "princess" in answer or 'fiona' in answer:
+            respond = "the princess will be up in the stairs in the highest room in the tallest tower"
+        elif "ogre" in answer:
+            respond = "ogres are like onions. onions have layers. ogres have layers"
+        else:
+            respond = random.choice(response)
+        
+        print(respond)
