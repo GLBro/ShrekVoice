d5553b1fd6b20899e4a0c9e72382f40ef63ec320
Your Name <you@example.com>
2019-08-07 10:32:03 +0100

    jh lh

--- shrek.py
+++ shrek.py
@@ -20,8 +20,7 @@ def handle_input(answer):
         respond = random.choice(rad)
     elif "story" in answer: 
        respond= playsound("soundclips/movie quotes (sound)/storyofshrek.mp3")
-
-        respond= "Once upon a time, there was a lovely princess. But she had an enchantment upon her of a fearful sort which could only be broken by love's first kiss. She was locked away in a castle guarded by a terrible fire-breathing dragon. Many brave knights had attempted to free her from this dreadful prison, but none prevailed. She waited in the dragon's keep, in the highest room of the tallest tower, for her true love, and true love's first kiss."
+    respond= "Once upon a time, there was a lovely princess. But she had an enchantment upon her of a fearful sort which could only be broken by love's first kiss. She was locked away in a castle guarded by a terrible fire-breathing dragon. Many brave knights had attempted to free her from this dreadful prison, but none prevailed. She waited in the dragon's keep, in the highest room of the tallest tower, for her true love, and true love's first kiss."
     elif "remind" in answer:
         respond = "its on my to do list"
     elif "princess" in answer or 'fiona' in answer or "where" in answer:
