762537d7a12bafb4a1c53ce46dca6ffa35cbbe84
GLBro <glebon2nrt@nsix.org.uk>
2019-08-09 09:36:20 +0100

    added cruel joke

--- shrekmenu.py
+++ shrekmenu.py
@@ -267,10 +267,10 @@ def handle_input(answer, word):
             root.update()
             engine.say('No location found')
             engine.runAndWait()
-            respond = 'To check the weather you must type \'what is the weather in (postcode)'
+            respond = 'To check the weather you must type \'what is the weather in (postcode)\''
     
     elif 'joke' in answer:
-        rad = '1', '2', '3', '4'
+        rad = '1', '2', '3', '4', '5'
         sayjoke = random.choice(rad)
         if sayjoke == '1':
             output.destroy()
@@ -308,6 +308,8 @@ def handle_input(answer, word):
             engine.say('What did shrek propose to fiona with?')
             engine.runAndWait()
             respond = 'An onion ring!'
+        if sayjoke == '5':
+            respond = 'YOU ARE A JOKE!'
         
 
 
